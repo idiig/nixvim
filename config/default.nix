72c98e4a1e8e7851807a4dd8b120e5051da5f2d0
idiig <avionplat@hotmail.com>
2024-05-28 01:14:18 +0900

    update constructions

--- config/default.nix
+++ config/default.nix
@@ -2,10 +2,7 @@
   # Import all your configuration modules here
   imports = [ 
     # ./bufferline.nix
-    ./core/keymaps.nix
-    ./core/options.nix
-
-    ./ui/colorschemes.nix
-    ./ui/btw.nix
+    ./core/default.nix
+    ./plugins/default.nix
   ];
 }
---
+++ config/plugins/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./ui/colorschemes.nix
+    ./ui/btw.nix
+  ];
+}
