e3a647d408643264177782814aa07496bd5023e5
idiig <avionplat@hotmail.com>
2024-05-28 21:43:35 +0900

    update formatter.nix

--- config/plugins/prog/formatter.nix
+++ config/plugins/prog/formatter.nix
@@ -1,32 +1,41 @@
 { config, lib, pkgs, ... }:
 
 {
-  plugins.conform-vim = {
+  plugins.conform-nvim = {
     enable = true;
 
-    format_on_save = {
-      lsp_fallback = true;
-      async = false;
-      timeout_ms = 1000;
+    formatOnSave = {
+      lspFallback = true;
+      # async = false;
+      timeoutMs = 1000;
     };
 
-    extraConfig = {
-      formatters_by_ft = {
-        javascript = { "prettier" };
-        typescript = { "prettier" };
-        javascriptreact = { "prettier" };
-        typescriptreact = { "prettier" };
-        svelte = { "prettier" };
-        css = { "prettier" };
-        php = { "prettier" };
-        html = { "prettier" };
-        json = { "prettier" };
-        yaml = { "prettier" };
-        markdown = { "prettier" };
-        graphql = { "prettier" };
-        lua = { "stylua" };
-        python = { "isort"; "black" };
-      };
+    formattersByFt = {
+      # Use the "*" filetype to run formatters on all filetypes.
+      "*" = [ "codespell" ];
+      # Use the "_" filetype to run formatters on filetypes that don't
+      # have other formatters configured.
+      "_" = [ "trim_whitespace" ];
+
+      javascript = [ "prettier" ];
+      typescript = [ "prettier" ];
+      javascriptreact = [ "prettier" ];
+      typescriptreact = [ "prettier" ];
+      svelte = [ "prettier" ];
+      css = [ "prettier" ];
+      php = [ "prettier" ];
+      html = [ "prettier" ];
+      json = [ "prettier" ];
+      yaml = [ "prettier" ];
+      markdown = [ "prettier" ];
+      graphql = [ "prettier" ];
+      lua = [ "stylua" ];
+      python = [ "isort" "black" ];
+    };
+
+    extraOptions = {
+      lazy = true;
+      event = [ "BufReadPre" "BufNewFile" ]; # to disable, comment this out
     };
   };
 
@@ -37,6 +46,7 @@
         async = false,
         timeout_ms = 1000,
       })
+    end, { desc = "Format file or range (in visual mode)" })
   '';
 
   # keymaps = [
@@ -47,39 +57,3 @@
   #   }
   # ];
 }
-
-# return {
-#   "stevearc/conform.nvim",
-#   lazy = true,
-#   event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-#   config = function()
-#     local conform = require("conform")
-
-#     conform.setup({
-#       formatters_by_ft = {
-#         javascript = { "prettier" },
-#         typescript = { "prettier" },
-#         javascriptreact = { "prettier" },
-#         typescriptreact = { "prettier" },
-#         svelte = { "prettier" },
-#         css = { "prettier" },
-#         php = { "prettier" },
-#         html = { "prettier" },
-#         json = { "prettier" },
-#         yaml = { "prettier" },
-#         markdown = { "prettier" },
-#         graphql = { "prettier" },
-#         lua = { "stylua" },
-#         python = { "isort", "black" },
-#       },
-#     })
-
-#     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-#       conform.format({
-#         lsp_fallback = true,
-#         async = false,
-#         timeout_ms = 1000,
-#       })
-#     end, { desc = "Format file or range (in visual mode)" })
-#   end,
-# }
