048bdf441787444bb65c57182b1449a356fe4d2f
idiig <avionplat@hotmail.com>
2024-05-30 15:44:47 +0900

    update options

--- config/core/options.nix
+++ config/core/options.nix
@@ -1,20 +1,25 @@
+{ pkgs, lib, ... }:
 {
-  options = {
+  config.opt = {
+
     encoding = "utf-8";
     scriptencoding = "utf-8";
     ambiwidth = "double";
+
     relativenumber = true; # show relative line numbers
     number = true; # shows absolute line number on cursor line (when relative number is on)
     scrolloff = 999;
+
     tabstop = 2; # 2 spaces for tabs (prettier default)
     shiftwidth = 2; # 2 spaces for indent width
     expandtab = true; # expand tab to spaces
     autoindent = true; # copy indent from current line when starting new one
-    wrap = true; # enable line wrapping
+    wrap = false; # unenable line wrapping
     ignorecase = true; # ignore case when searching
     smartcase = true; # if you include mixed case in your search, assumes you want case-sensitive
     cursorline = true; # highlight the current cursor line
-    termguicolors = true; # enable true color support
+    smartindent = true;
+
     background = "dark"; # colorschemes that can be light or dark will be made dark
     signcolumn = "yes"; # show sign column so that text doesn't shift
     backspace = "indent,eol,start"; # allow backspace on indent, end of line or insert mode start position
@@ -22,8 +27,14 @@
     splitbelow = true; # split horizontal window to the bottom
     swapfile = false; # disable swapfile
     helplang = ["ja"]; # set help language to Japanese
-    clipboard = {
-      unnamedplus = true; # use system clipboard as default register
-    };
+
+    clipboard = "unnamedplus";
+
+    incsearch = true;
+    wildmode = "list:longest";
+
+    undofile = true; # Build-in persistent undo
+
+    termguicolors = lib.mkForce pkgs.stdenv.isLinux;
   };
 }
